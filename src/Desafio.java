7746bd996a7fb8a4d74b870063d6bea778eeb596
ranndyy <ranndy180@gmail.com>
2024-09-16 20:47:42 -0500

    primera actualizacion 1.0

--- src/Desafio.java
+++ src/Desafio.java
@@ -2,7 +2,9 @@
 
 import java.util.Scanner;
 
-public class Desafio {
+import static java.lang.Integer.parseInt;
+
+public class  Desafio {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
@@ -21,33 +23,37 @@ public class Desafio {
         int i = 0;
         while (i != 1){
 
-            
+
             System.out.println("eligue una de las opciones");
             String opciones = """
-                    1: Consultar saldo
-                    2: Retirar 
-                    3: Depositar
-                    4: Salir""";
+                    1: Consultar saldo.
+                    2: Retirar.
+                    3: Depositar.
+                    4: Salir.""";
             System.out.println(opciones);
-            
+
             int opcionEleguida = teclado.nextInt();
             if (opcionEleguida == 1) {
                 System.out.println("Tu saldo disponible es: " + saldoDisponible + "$");
+                System.out.println("");
             }
             else if (opcionEleguida == 2) {
-                  if (saldoDisponible == 0) {
-                      System.out.println("no tienes saldo para retirar");
-                  }else {
-                      System.out.println("Escribe el saldo que vas a retirar");
-                      int saldoRetirado = teclado.nextInt();
-                      saldoDisponible = saldoDisponible - saldoRetirado;
-                      System.out.println("Tualdo nuevo es: " + saldoDisponible);
-                  }
+                if (saldoDisponible == 0) {
+                    System.out.println("no tienes saldo para retirar");
+                    System.out.println("");
+                }else {
+                    System.out.println("Escribe el saldo que vas a retirar");
+                    int saldoRetirado = teclado.nextInt();
+                    saldoDisponible = saldoDisponible - saldoRetirado;
+                    System.out.println("Tualdo nuevo es: " + saldoDisponible);
+                    System.out.println("");
+                }
             } else if (opcionEleguida == 3) {
                 System.out.println("Digita la cantidad que quieres ingresar");
                 int saldoIngresado = teclado.nextInt();
                 saldoDisponible = saldoDisponible + saldoIngresado;
                 System.out.println("Tu saldo disponible es: " + saldoDisponible + "$");
+                System.out.println("");
 
             } else if (opcionEleguida == 4) {
                 System.out.println("informacion final");
@@ -56,10 +62,7 @@ public class Desafio {
                 System.out.println(saldoDisponible);
                 i = 1;
 
-            }else {
-                System.out.println("No reconosco la opcion que elegiste");
             }
-
         }
     }
 }
