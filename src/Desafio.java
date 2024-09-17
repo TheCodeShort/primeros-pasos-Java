b1b792390e7e99c9a92ef391f9aac71349889b7d
ranndy <ranndy180@gmail.com>
2024-09-16 21:25:16 -0500

    final pero posible mejoramiento

--- src/Desafio.java
+++ src/Desafio.java
@@ -12,7 +12,7 @@ public class  Desafio {
         String nombreUsuario = teclado.nextLine();
         System.out.println("que tipo de cuenta tienes ahorros o credito:");
         String tipoDeCuenta = teclado.nextLine();
-        int saldoDisponible = 0;
+        double saldoDisponible = 0;
         System.out.println("");
 
         System.out.println("Nombre de usuario:" + nombreUsuario);
@@ -38,15 +38,21 @@ public class  Desafio {
                 System.out.println("");
             }
             else if (opcionEleguida == 2) {
+
                 if (saldoDisponible == 0) {
                     System.out.println("no tienes saldo para retirar");
                     System.out.println("");
-                }else {
+                } else {
                     System.out.println("Escribe el saldo que vas a retirar");
-                    int saldoRetirado = teclado.nextInt();
-                    saldoDisponible = saldoDisponible - saldoRetirado;
-                    System.out.println("Tualdo nuevo es: " + saldoDisponible);
-                    System.out.println("");
+                    double saldoRetirado = teclado.nextDouble();
+                    if (saldoRetirado > saldoDisponible) {
+                        System.out.println("Monto de retiro no disponible");
+                        System.out.println("");
+                    }else {
+                        saldoDisponible = saldoDisponible - saldoRetirado;
+                        System.out.println("Tualdo nuevo es: " + saldoDisponible);
+                        System.out.println("");
+                    }
                 }
             } else if (opcionEleguida == 3) {
                 System.out.println("Digita la cantidad que quieres ingresar");
@@ -62,6 +68,8 @@ public class  Desafio {
                 System.out.println(saldoDisponible);
                 i = 1;
 
+            }else{
+                System.out.println("obcion no valida");
             }
         }
     }
